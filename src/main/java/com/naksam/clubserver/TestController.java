bd8e46d6403b8d7d857a845712e83f156084b6d6
강민형 <56009774+rkdals213@users.noreply.github.com>
2021-10-19 00:32:51 +0900

    jpa 설정 추가

--- src/main/java/com/naksam/clubserver/TestController.java
+++ src/main/java/com/naksam/clubserver/TestController.java
@@ -1,11 +1,9 @@
 package com.naksam.clubserver;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/club")
 public class TestController {
 
     @GetMapping("test")
