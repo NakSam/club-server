f33a0547f12b94e52119e4d70d410c31bbf3e189
rkdals213 <rkdals213@naver.com>
2021-10-18 17:30:23 +0900

    초기 설정

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM openjdk:1.8
-
-EXPOSE 8080
-
-ADD build/libs/BarkingCat-0.0.1-SNAPSHOT.jar app.jar
-
-ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/TestController.java
+++ src/main/java/TestController.java
@@ -1,5 +1,3 @@
-package com.naksam.clubserver;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
