7f5b0dab2032cfffdf1748b07bdb5fadc85d82cb
rkdals213 <rkdals213@naver.com>
2021-10-19 09:43:46 +0900

    설정 추가

--- src/main/java/com/naksam/clubserver/Application.java
+++ src/main/java/com/naksam/clubserver/Application.java
@@ -1,9 +1,13 @@
 package com.naksam.clubserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
 public class Application {
 
     public static void main(String[] args) {
