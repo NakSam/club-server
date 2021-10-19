78845da394d6914203fb4058a07e955cde27a198
rkdals213 <rkdals213@naver.com>
2021-10-19 12:27:27 +0900

    querydsl 설정 추가

---
+++ src/main/java/com/naksam/clubserver/data/ClubQueryRepository.java
@@ -0,0 +1,18 @@
+package com.naksam.clubserver.data;
+
+import com.naksam.clubserver.domain.QClub;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClubQueryRepository {
+    private JPAQueryFactory query;
+
+    public void test() {
+        query.selectFrom(QClub.club).fetch();
+    }
+}
