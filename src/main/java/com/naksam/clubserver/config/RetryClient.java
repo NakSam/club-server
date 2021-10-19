23b17de3424840a9ce9e215426eb4b09bc013667
강민형 <56009774+rkdals213@users.noreply.github.com>
2021-10-19 21:03:08 +0900

    feat: 회원 인증 서버 연동 테스트

--- src/main/java/com/naksam/clubserver/config/RetryClient.java
+++ src/main/java/com/naksam/clubserver/config/RetryClient.java
@@ -2,7 +2,7 @@ package com.naksam.clubserver.config;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(value = "retry", url = "account:8080/", configuration = {FeignConfiguration.class, FeignRetryConfiguration.class})
+@FeignClient(value = "retry", url = "http://169.56.174.139/account", configuration = {FeignConfiguration.class, FeignRetryConfiguration.class})
 public interface RetryClient extends ExampleClient {
 
 }
