20548d0fa6f290036694897926ef46811682a83b
강민형 <56009774+rkdals213@users.noreply.github.com>
2021-10-19 16:52:20 +0900

    Update ClubController.java

--- src/main/java/com/naksam/clubserver/presentation/ClubController.java
+++ src/main/java/com/naksam/clubserver/presentation/ClubController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ClubController {
     private ClubService clubService;
 
