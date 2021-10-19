447351a4fe8044fcbaa01c0d487de214af18c2bf
rkdals213 <rkdals213@naver.com>
2021-10-19 13:08:03 +0900

    exclude 수정

--- src/main/java/com/naksam/clubserver/Application.java
+++ src/main/java/com/naksam/clubserver/Application.java
@@ -2,10 +2,9 @@ package com.naksam.clubserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication//(exclude = {HibernateJpaAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
