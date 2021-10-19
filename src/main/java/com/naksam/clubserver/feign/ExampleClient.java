311e1883376c588eca504c9f758ca1e9bb90c629
강민형 <56009774+rkdals213@users.noreply.github.com>
2021-10-19 22:42:34 +0900

    clean: 불필요 코드 제거

--- src/main/java/com/naksam/clubserver/feign/ExampleClient.java
+++ src/main/java/com/naksam/clubserver/feign/ExampleClient.java
@@ -1,13 +1,12 @@
 package com.naksam.clubserver.feign;
 
-import com.naksam.clubserver.config.HeaderConfiguration;
 import com.naksam.clubserver.dto.JsonWebToken;
 import com.naksam.clubserver.dto.MemberPayload;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "example", url = "http://169.56.174.139/account", configuration = {HeaderConfiguration.class})
+@FeignClient(value = "example", url = "http://169.56.174.139/account")
 public interface ExampleClient {
 
     @PostMapping(value = "/create")
