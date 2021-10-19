fedbb7cb3c873582e86812f413d247a98b525572
wnsgud245 <wnsgud245@gmail.com>
2021-10-19 10:41:39 +0900

    feat(club): EnableJpaRepositories 어노테이션 추가

--- src/main/java/com/naksam/clubserver/Application.java
+++ src/main/java/com/naksam/clubserver/Application.java
@@ -3,9 +3,10 @@ package com.naksam.clubserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class})
+@EnableJpaRepositories
 public class Application {
 
     public static void main(String[] args) {
