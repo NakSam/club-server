9a0b8335d8c76837caba6d7496fdcb39f82c044c
wnsgud245 <wnsgud245@gmail.com>
2021-10-19 15:37:12 +0900

    feat(모임 검색): clubRepo 함수 제거

--- src/main/java/com/naksam/clubserver/data/ClubRepository.java
+++ src/main/java/com/naksam/clubserver/data/ClubRepository.java
@@ -1,13 +1,9 @@
 package com.naksam.clubserver.data;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.naksam.clubserver.domain.Club;
-import com.naksam.clubserver.domain.constants.Category;
-import com.naksam.clubserver.domain.constants.Location;
 
 public interface ClubRepository extends JpaRepository<Club, Long> {
-    List<Club> findAllByLocationAndCategory(Location location, Category category);
 }
