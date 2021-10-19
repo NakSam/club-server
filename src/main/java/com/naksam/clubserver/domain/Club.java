6b1f093565a50f5b76532ced93f1ff07a4da572f
rkdals213 <rkdals213@naver.com>
2021-10-19 13:51:35 +0900

    club 엔티티 수정

--- src/main/java/com/naksam/clubserver/domain/Club.java
+++ src/main/java/com/naksam/clubserver/domain/Club.java
@@ -1,14 +1,15 @@
 package com.naksam.clubserver.domain;
 
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Club {
 
     @Id
@@ -20,42 +21,15 @@ public class Club {
 
     private Long ownerId;
 
-    private long memberNum;
+    private Long memberNum;
 
-    private long maxMemberNum;
+    private Long maxMemberNum;
 
     private String category;
 
     private String location;
 
-    public Long getId() {
+    public Long id() {
         return id;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public Long getOwnerId() {
-        return ownerId;
-    }
-
-    public long getMemberNum() {
-        return memberNum;
-    }
-
-    public long getMaxMemberNum() {
-        return maxMemberNum;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-
-
 }
--- src/main/java/com/naksam/clubserver/domain/ClubDomain.java
+++ src/main/java/com/naksam/clubserver/domain/ClubDomain.java
@@ -1,13 +1,11 @@
 package com.naksam.clubserver.domain;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import com.naksam.clubserver.data.ClubRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @AllArgsConstructor
@@ -15,7 +13,6 @@ import lombok.NoArgsConstructor;
 public class ClubDomain {
 
     private ClubRepository clubRepository;
-
 
     public List<Club> search(String location, String category) {
         return clubRepository.findAllByCategoryAndLocation(location, category);
