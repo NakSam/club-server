92fad4b3aa86de7ea9a7d29e87aa931a1ee03898
wnsgud245 <wnsgud245@gmail.com>
2021-10-19 10:46:47 +0900

    feat(club): exclude hibernate 주석처리 제외, domain repository autowired 추가

--- src/main/java/com/naksam/clubserver/domain/ClubDomain.java
+++ src/main/java/com/naksam/clubserver/domain/ClubDomain.java
@@ -2,12 +2,14 @@ package com.naksam.clubserver.domain;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.naksam.clubserver.data.ClubRepository;
 
 @Component
 public class ClubDomain {
+    @Autowired
     private final ClubRepository clubRepository;
 
     public ClubDomain(ClubRepository clubRepository) {
