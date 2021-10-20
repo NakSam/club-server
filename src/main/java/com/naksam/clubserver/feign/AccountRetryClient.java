643f47a6083c5ab2f7f4efe95d7e5d7a8a1b49a5
rkdals213 <rkdals213@naver.com>
2021-10-20 16:43:33 +0900

    fix: url 수정

--- src/main/java/com/naksam/clubserver/feign/AccountRetryClient.java
+++ src/main/java/com/naksam/clubserver/feign/AccountRetryClient.java
@@ -4,7 +4,7 @@ import com.naksam.clubserver.config.FeignConfiguration;
 import com.naksam.clubserver.config.FeignRetryConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(value = "retry", url="${ACCOUNT_HOST:http://169.56.174.139/account}", configuration = {FeignConfiguration.class, FeignRetryConfiguration.class})
+@FeignClient(value = "retry", url="${ACCOUNT_HOST:http://naksam.169.56.174.130.nip.io/account}", configuration = {FeignConfiguration.class, FeignRetryConfiguration.class})
 public interface AccountRetryClient extends AccountClient {
 
 }
