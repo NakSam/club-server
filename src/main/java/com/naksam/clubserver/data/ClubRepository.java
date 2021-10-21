39078ba67adc88d604ca89eba571f3074a74d0e9
wnsgud245 <wnsgud245@gmail.com>
2021-10-21 10:20:32 +0900

    feat(home): 최신 5개 모임 정보 리스트 조회 API

--- src/main/java/com/naksam/clubserver/data/ClubRepository.java
+++ src/main/java/com/naksam/clubserver/data/ClubRepository.java
@@ -1,9 +1,12 @@
 package com.naksam.clubserver.data;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.naksam.clubserver.domain.entity.Club;
 
 public interface ClubRepository extends JpaRepository<Club, Long> {
+    public List<Club> findTop5ByOrderByCreatedTimeDesc();
 }
