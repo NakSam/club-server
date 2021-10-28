9d482f5c15d67b1876ff0856ae060402995ee9ef
rkdals213 <rkdals213@naver.com>
2021-10-28 14:48:56 +0900

    test

--- src/main/java/com/naksam/clubserver/common/HttpSupport.java
+++ src/main/java/com/naksam/clubserver/common/HttpSupport.java
@@ -7,6 +7,8 @@ import java.util.stream.Stream;
 
 public class HttpSupport {
     public static Optional<Cookie> getCookie(HttpServletRequest req, String name) {
+        String naksam = req.getHeader("naksam");
+        System.out.println(naksam);
         return Stream.of(req.getCookies())
                 .filter(cookie -> name.equals(cookie.getName()) && !cookie.getValue()
                         .isEmpty())
